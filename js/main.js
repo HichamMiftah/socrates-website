b0ec5fd3e3ae94c2941c2fedcab2eb24f2bb8c20
HichamMiftah <hichammiftah8004@gmail.com>
2022-12-19 21:34:47 +0100

    first commit project finished

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(window).on("load", function () {
+    "use strict";
+
+    // RESPONSIVE MOBILE MENU 
+
+    $(".menu-btn").on("click", function () {
+        $(".responsive-mobile-menu").toggleClass("show");
+        return false;
+    });
+
+    $("html").on("click", function () {
+        $(".responsive-mobile-menu").removeClass("show");
+    });
+    $(".menu-btn, .responsive-mobile-menu").on("click", function (e) {
+        e.stopPropagation();
+    })
+
+});
+
